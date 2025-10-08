b1589db278d966725293a732bff6a99427840636
Dannalyze <danielace007.exe@gmail.com>
2025-10-08 10:46:40 +0100

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,3 @@
+module.exports = ({ title, author, genre, style }) => {
+  return `Book cover for "${title}" by ${author}. Genre: ${genre}. Style: ${style}. High detail, cinematic lighting, dramatic composition.`;
+};
