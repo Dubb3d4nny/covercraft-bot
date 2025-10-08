c655ece816b5aa2a8433a2721e63ca697f8225dc
Dannalyze <danielace007.exe@gmail.com>
2025-10-08 21:59:49 +0100

    Delete utils.js

--- utils.js
+++
@@ -1,3 +0,0 @@
-module.exports = ({ title, author, genre, style }) => {
-  return `Book cover for "${title}" by ${author}. Genre: ${genre}. Style: ${style}. High detail, cinematic lighting, dramatic composition.`;
-};
