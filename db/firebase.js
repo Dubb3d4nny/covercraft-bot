ecfe110be25cbeb843e025d638dd538ff1c4f7a4
Dannalyze <danielace007.exe@gmail.com>
2025-10-08 23:46:26 +0100

    Update firebase.js

--- db/firebase.js
+++ db/firebase.js
@@ -1,15 +1,20 @@
+// db/firebase.js
+
+// In-memory user storage (temporary for testing)
+if (!global.users) global.users = {};
+
+// Get user info
 exports.getUser = async (telegramId) => {
-  if (!global.users) global.users = {};
-  if (!global.users[telegramId]) global.users[telegramId] = { creditsUsed: 0, balance: 0 };
+  if (!global.users[telegramId]) {
+    global.users[telegramId] = { creditsUsed: 0, balance: 0 };
+  }
   return global.users[telegramId];
 };
 
+// Increment user credits
 exports.incrementCredits = async (telegramId) => {
-  if (!global.users[telegramId]) global.users[telegramId]) global.users[telegramId] = { creditsUsed: 0, balance: 0 };
+  if (!global.users[telegramId]) {
+    global.users[telegramId] = { creditsUsed: 0, balance: 0 };
+  }
   global.users[telegramId].creditsUsed += 1;
-};
-
-exports.addBalance = async (telegramId, amount) => {
-  if (!global.users[telegramId]) global.users[telegramId]) global.users[telegramId] = { creditsUsed: 0, balance: 0 };
-  global.users[telegramId].balance += amount;
 };
